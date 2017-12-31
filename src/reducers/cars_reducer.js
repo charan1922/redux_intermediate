e71de029608df8d6bfe4fbef3a0a509b9b8e4ca6
Charan Teja <charaneie22@gmail.com>
2017-12-31 13:10:49 +0530

    updated reducer

---
+++ src/reducers/cars_reducer.js
@@ -0,0 +1,16 @@
+export default function(state = null, action) {
+  // switch (action.type) {
+
+
+  //   case "SEARCH_CARS":
+  //   console.log(action.payload);
+  //     return action.payload;
+
+  //   default:
+  //     return state;
+  // }
+
+
+  console.log(action)
+  return state;
+}
