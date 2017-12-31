d58c7f610a73c11db3d81117bb5b97724ba58224
Charan Teja <charaneie22@gmail.com>
2017-12-31 13:12:10 +0530

    imported reducers and created store

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//import provider
+import {Provider} from 'react-redux';
+
+//import store and middleware
+import {createStore , applyMiddleware} from 'redux';
+
 //import styles
 import './app.css';
 
+//import reducers
+import reducers from './reducers'
+
+//import route
 import {BrowserRouter, Route} from 'react-router-dom'
  
 //import components
 import App from './components/app';
 import Car from './components/car';
 
-ReactDOM.render(
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+
+ReactDOM.render( <Provider store={createStoreWithMiddleware(reducers)} > 
 <BrowserRouter>
 <div>
     <Route exact path="/" component={App} > </Route>
     <Route path="/car/:id" component={Car} > </Route>
 </div>
 </BrowserRouter>
+</Provider>
 
 , document.getElementById('root'));
 
