06810bb14fff5af3d9ad672e70ca06bde5f1e56c
Charan Teja <charaneie22@gmail.com>
2018-01-01 02:13:00 +0530

    addedcase statements

--- src/reducers/cars_reducer.js
+++ src/reducers/cars_reducer.js
@@ -1,16 +1,10 @@
-export default function(state = null, action) {
-  // switch (action.type) {
+export default function(state = [], action) {
+  switch (action.type) {
+    case "SEARCH_CARS":
+      console.log(action.payload);
+      return action.payload;
 
-
-  //   case "SEARCH_CARS":
-  //   console.log(action.payload);
-  //     return action.payload;
-
-  //   default:
-  //     return state;
-  // }
-
-
-  console.log(action)
-  return state;
+    default:
+      return state;
+  }
 }
